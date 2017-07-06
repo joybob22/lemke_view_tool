729259aee41ed283410c302745439f46937a4b38
joybob22 <braydenlemke8@gmail.com>
2017-07-06 21:14:54 +0000

    initial commit

---
+++ lib/lemke_view_tool.rb
@@ -0,0 +1,6 @@
+require "lemke_view_tool/version"
+require "lemke_view_tool/renerer"
+
+module LemkeViewTool
+  # Your code goes here...
+end
---
+++ lib/lemke_view_tool/version.rb
@@ -0,0 +1,3 @@
+module LemkeViewTool
+  VERSION = "0.1.0"
+end
